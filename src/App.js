201e329da5f3e109fb5d7c0ee515ad9af117777f
Marshall Oh <dhtmdgusxx@naver.com>
2021-01-28 11:23:26 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
+import Customer from "./components/Customer";
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: "http://placeimg.com/64/64/1",
+    name: "나동빈",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "http://placeimg.com/64/64/2",
+    name: "홍길동",
+    birthday: "960305",
+    gender: "남자",
+    job: "프로그래머",
+  },
+  {
+    id: 3,
+    image: "http://placeimg.com/64/64/3",
+    name: "오세훈",
+    birthday: "921205",
+    gender: "남자",
+    job: "프로그래머",
+  },
+];
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </>
+    );
+  }
 }
-
-export default App;
